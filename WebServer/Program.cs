1f87a5bbc19562c9b67977ab196b737f35b4df3e
tavisca-abchauhan <achauhan@tavisca.in>
2019-08-08 12:13:38 +0530

    Review

---
+++ WebServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Strarting the server");
+            HttpServer server = new HttpServer(8000);
+            server.Start();
+        }
+    }
+}
---
+++ WebServer/Request.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServer
+{
+    class Request
+    {
+        public string Type { get; set; }
+        public string URL { get; set; }
+        public string Host{ get; set; }
+        public Request(string type,string url,string host)
+        {
+            Type = type;
+            URL = url;
+            Host = host;
+        }
+        public static Request GetRequest(string request)
+        {
+            if(string.IsNullOrEmpty(request))
+            {
+                return null;
+
+            }
+            string[] tokens = request.Split(' ');
+            string type = tokens[0];
+            string url = tokens[1];
+            string host = "localhost";
+
+            return new Request(type,url,host);
+        }
+        
+    }
+}
